6bd9cc3577f5bd637ec3b963ba12f7d83a8c1399
igorgncampos <igorgncampos@gmail.com>
2022-08-15 21:53:17 -0300

    Envio inicial do codigo.
    Envio inicial do codigo.

---
+++ AbrirNavegador/BrowserDriver.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbrirNavegador
+{
+
+    public class BrowserDriver
+    {
+        public IWebDriver driver;
+        public BrowserDriver() 
+        {
+            ChromeOptions opt = new ChromeOptions();
+
+            opt.AddExcludedArgument("enable-automation");
+            //opt.AddAdditionalOption("useAutomationExtension", false);
+            opt.AcceptInsecureCertificates = true;
+            opt.AddArgument("--start-maximized");
+            opt.AddArgument("--aways-authorize-plugins");
+            opt.AddArgument("--disable-notifications");
+            opt.AddArgument("--no-sandbox");
+            opt.AddArgument("--ignore-certificate-errors");
+            opt.AddArgument("--ignore-ssl-errors");
+            
+
+            //opt.AddArgument("--headless");
+
+            ChromeDriverService chromeDriverService =
+            ChromeDriverService.CreateDefaultService($@"C:\Program Files\Google\Chrome\Application");
+
+            driver = new ChromeDriver(chromeDriverService, opt);
+        }
+
+        public void TarefaNavegador()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.FindElement(By.Name("q")).Click();
+           
+            driver.FindElement(By.Name("q")).Clear();
+            driver.FindElement(By.Name("q")).SendKeys("pokemon");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+        }
+    }
+
+
+}
---
+++ AbrirNavegador/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+
+namespace AbrirNavegador
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BrowserDriver browser = new BrowserDriver();
+            //ChromeOptions opt = new ChromeOptions();
+            browser.TarefaNavegador();
+
+        }
+    }
+}
